9aae0d76f381071cd1bcc0afd0b28ae5380599d1
ZakFahey <z.fahey@gmail.com>
2018-11-29 18:12:15 -0500

    Fixed URL

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 module.exports = withSass({
     webpack(config, options) {
         config.plugins.push(new webpack.DefinePlugin({
-            API_LOCATION: JSON.stringify(process.env.API_LOCATION ? process.env.API_LOCATION : "localhost:8000"),
+            API_LOCATION: JSON.stringify(process.env.API_LOCATION ? process.env.API_LOCATION : "http://localhost:8000"),
             WEBSITE_NAME: JSON.stringify(process.env.WEBSITE_NAME ? process.env.WEBSITE_NAME : "thegift.com"),
             USE_MOCK_API: process.env.USE_MOCK_API
         }));
